8b7d5831841f5f481285d39c2f5c6f45ca6eee1a
tom-2810 <tom.poortman@hva.nl>
2023-06-21 12:19:36 +0200

    Update flight_api.js

--- flight_api.js
+++ flight_api.js
@@ -1,28 +1,28 @@
-var http = require("https");
+// var http = require("https");
 
-var options = {
-  "method": "GET",
-  "hostname": "https://api.schiphol.nl/public-flights",
-  "port": null,
-  "path": "/flights",
-  "headers": {
-    "resourceversion": "v4",
-	"app_id": "1822ff3f",
-	"app_key": "3e9da2fd144c0ffaacc1bd575f4f39ab"
-  }
-};
+// var options = {
+//   "method": "GET",
+//   "hostname": "https://api.schiphol.nl/public-flights",
+//   "port": null,
+//   "path": "/flights",
+//   "headers": {
+//     "resourceversion": "v4",
+// 	"app_id": "1822ff3f",
+// 	"app_key": "3e9da2fd144c0ffaacc1bd575f4f39ab"
+//   }
+// };
 
-var req = http.request(options, function (res) {
-  var chunks = [];
+// var req = http.request(options, function (res) {
+//   var chunks = [];
 
-  res.on("data", function (chunk) {
-    chunks.push(chunk);
-  });
+//   res.on("data", function (chunk) {
+//     chunks.push(chunk);
+//   });
 
-  res.on("end", function () {
-    var body = Buffer.concat(chunks);
-    console.log(body.toString());
-  });
-});
+//   res.on("end", function () {
+//     var body = Buffer.concat(chunks);
+//     console.log(body.toString());
+//   });
+// });
 
-req.end();
+// req.end();
